901ceef79cae5c8d59d4a8f83c2dc7ad66f0b597
Dave Franco <davefranco1987@gmail.com>
2022-02-23 11:44:36 +0100

    triggered by s3 batch ops

---
+++ lambda/triggered_by_s3batch.py
@@ -0,0 +1,69 @@
+import os
+import s3mv
+from urllib import parse
+
+def lambda_handler(event, context):
+    """
+    Main entrypoint
+    """
+    
+    """
+    {
+        "invocationSchemaVersion": "1.0",
+        "invocationId": "YXNkbGZqYWRmaiBhc2RmdW9hZHNmZGpmaGFzbGtkaGZza2RmaAo",
+        "job": {
+            "id": "f3cc4f60-61f6-4a2b-8a21-d07600c373ce"
+        },
+        "tasks": [
+            {
+                "taskId": "dGFza2lkZ29lc2hlcmUK",
+                "s3Key": "customerImage1.jpg",
+                "s3VersionId": "1",
+                "s3BucketArn": "arn:aws:s3:us-east-1:0123456788:awsexamplebucket1"
+            }
+        ]
+    }
+
+    """
+    # Parse job parameters from Amazon S3 batch operations
+    invocation_id = event['invocationId']
+    invocation_schema_version = event['invocationSchemaVersion']
+
+    results = []
+    result_code = None
+    result_string = None
+
+    task = event['tasks'][0]
+    task_id = task['taskId']
+
+    bucket = task['s3BucketArn'].split(':')[-1]
+    obj_key = parse.unquote(task['s3Key'], encoding='utf-8')
+
+    if s3mv.has_tags(bucket, obj_key, s3mv.TAGS):
+        try:
+            s3mv.move_object(bucket, os.environ['TARGET_BUCKET'], obj_key)
+            result_code = 'Succeeded'
+            result_string = f"Successfully moved object " \
+                                    f"{obj_key} from bucket {bucket}."
+        except Exception as err:
+            result_code = "PermanentFailure"
+            result_string = f"Unable to move object " \
+                                    f"{obj_key} from bucket {bucket}."
+            raise
+    else: 
+        result_code = 'TemporaryFailure'
+        result_string = f"object {obj_key} from bucket {bucket}" \
+                                    f"does not contain the searched tag."
+
+    results.append({
+        'taskId': task_id,
+        'resultCode': result_code,
+        'resultString': result_string
+    })
+    return {
+        "invocationSchemaVersion": invocation_schema_version,
+        "treatMissingKeysAs" : "PermanentFailure",
+        "invocationId": invocation_id,
+        "results": results
+    }
+    
